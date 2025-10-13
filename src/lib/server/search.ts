7807d91cb2dedf36c90339238ef53e9a95aa91c9
Damon <57426668+BruhSoundEffectNumber2@users.noreply.github.com>
2025-10-12 22:06:40 -0700

    Improved search to meet tests

--- src/lib/server/search.ts
+++ src/lib/server/search.ts
@@ -2,9 +2,17 @@ import type { SearchResult } from "$lib";
 import { dummyStocks } from "./dummyStocks";
 
 export async function search(term: string): Promise<SearchResult[]> {
-    return dummyStocks.filter(
+    if (!term) {
+        throw new Error("Search term is required");
+    }
+
+    const lowerTerm = term.toLowerCase();
+
+    const results = await dummyStocks.filter(
         (stock) =>
-            stock.name.toLowerCase().includes(term) ||
-            stock.symbol.toLowerCase().includes(term)
+            stock.name.toLowerCase().includes(lowerTerm) ||
+            stock.symbol.toLowerCase().includes(lowerTerm)
     );
+
+    return results.slice(0, 5);
 }
