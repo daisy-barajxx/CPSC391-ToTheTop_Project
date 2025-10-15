c02021e4aef09e08f16b336dd269bdb5bf179978
Damon <57426668+BruhSoundEffectNumber2@users.noreply.github.com>
2025-10-15 14:43:18 -0700

    Use dynamic env to prevent sv check error (hopefully)

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,11 +1,11 @@
-import { POSTGRES_USER, POSTGRES_PW } from "$env/static/private";
+import { env } from "$env/dynamic/private";
 import postgres from "postgres";
 
 const sql = postgres({
     database: "cpsc391_tttop_db",
     host: "cps-postgresql.gonzaga.edu",
-    user: POSTGRES_USER,
-    password: POSTGRES_PW,
+    user: env.POSTGRES_USER,
+    password: env.POSTGRES_PW,
 });
 
 export default sql;
