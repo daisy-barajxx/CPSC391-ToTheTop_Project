3d612d31ada7448f27832c8bb1aad625f4b5ab08
Daisy Barajas-Manzo <daisybarajas135@gmail.com>
2025-10-15 23:29:22 -0700

    dummy lines added/needed

--- src/routes/stocks/[symbol]/+page.server.ts
+++ src/routes/stocks/[symbol]/+page.server.ts
@@ -1,5 +1,17 @@
+import { error } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
+import { dummyStocks } from "$lib/server/dummyStocks";
 
 export const load: PageServerLoad = async ({ params }) => {
-    return { symbol: params.symbol };
+    const symbol = params.symbol.toUpperCase();
+    const stock = dummyStocks.find((s) => s.symbol === symbol);
+    
+    if (!stock) {
+        throw error(404, "Stock ${symbol} not found");
+    }
+
+    return{
+        stock,
+        symbol
+    };
 };
--- src/routes/stocks/search/+server.ts
+++ src/routes/stocks/search/+server.ts
@@ -2,14 +2,18 @@ import { json, type RequestHandler } from "@sveltejs/kit";
 import { search } from "$lib/server/search";
 
 export const GET: RequestHandler = async ({ url }) => {
-    const term = url.searchParams.get("term")?.toLowerCase() || "";
+    // const term = url.searchParams.get("term") || "";
 
     // Don't waste time searching with no search term
-    if (term == "") {
-        return json([]);
-    }
+    // if (term == "") {
+    //     return json([]);
+    // }
 
-    const results = await search(term);
+    // const results = await search(term);
 
+//lines 15 & 16 for dummy data 
+    const term = url.searchParams.get("term") || "";
+    const results = search(term);
+    
     return json(results);
 };
