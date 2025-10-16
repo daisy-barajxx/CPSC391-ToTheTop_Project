7da502e5d1487fffaf9f27bd7e4237cd8d2d6a41
Daisy Barajas-Manzo <daisybarajas135@gmail.com>
2025-10-16 11:03:24 -0700

    added a new interface called stock

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,11 @@
 export interface SearchResult {
     symbol: string;
     name: string;
+}
+
+export interface Stock {
+    symbol: string;
+    name: string;
     price: number;
     priceChange: number;
     percentChange: number;
