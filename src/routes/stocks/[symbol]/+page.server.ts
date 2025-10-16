43f93b64e4f07868b8f23ff0ec5c2968f7f1ef75
Daisy Barajas-Manzo <daisybarajas135@gmail.com>
2025-10-16 10:38:34 -0700

    only need to reroute to stock page

--- src/routes/stocks/[symbol]/+page.server.ts
+++ src/routes/stocks/[symbol]/+page.server.ts
@@ -1,5 +1,12 @@
 import type { PageServerLoad } from "./$types";
+import { dummyStocks } from "$lib/server/dummyStocks";
 
 export const load: PageServerLoad = async ({ params }) => {
-    return { symbol: params.symbol };
+	const symbol = params.symbol.toUpperCase();
+	const stock = dummyStocks.find((s) => s.symbol === symbol);
+
+    return { 
+		stock,
+		symbol 
+	};
 };
--- src/routes/stocks/[symbol]/+page.svelte
+++ src/routes/stocks/[symbol]/+page.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 
-	let { data }: { data: PageData } = $props();
+	let { data } = $props<{ data: PageData }>();
 	const { stock, symbol } = data;
-
 </script>
 
 <div>
 	<h1>{symbol}</h1>
 	<p>{stock.name}</p>
+
+	<div>
+		<h2>Price Chart</h2>
+		<p>Chart will render here</p>
+	</div>
 </div>
 
