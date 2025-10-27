8395fceef92020c0e7638f6b969aaf9fc3755e8b
jwrig <jwright9@zagmail.gonzaga.edu>
2025-10-27 01:42:46 -0700

    API stock history added

--- src/routes/stocks/[symbol]/+page.server.ts
+++ src/routes/stocks/[symbol]/+page.server.ts
@@ -3,6 +3,11 @@ import { dummyStocks } from "$lib/server/dummyStocks";
 import { error } from "@sveltejs/kit";
 import type { Stock, StockOHLC } from "$lib";
 
+import { GetStocksAggregatesSortEnum, GetStocksAggregatesTimespanEnum, restClient } from "@polygon.io/client-js";
+
+const apiKey = "KNugSCBuDCeuAw28nz3pgvpj8iopDqoz";
+const rest = restClient(apiKey, "https://api.polygon.io");
+
 interface RawOHLC {
   t: string;
   o: number;
@@ -15,13 +20,17 @@ export const load: PageServerLoad = async ({ params, fetch }) => {
     const symbol = params.symbol.toUpperCase();
 
     // TODO: Replace with real stock data
+    //const stock = getStockInfo(symbol);
+   
    const stock = dummyStocks.find((s) => s.symbol === symbol) as Stock | undefined;
   if (!stock) {
     throw error(400, "Stock does not exist.");
   }
 
     // TODO: Replace with real stock price history data
-    const priceHistoryRaw = await (await fetch("/aapl-ohlc-data.json")).json();
+    const priceHistoryRaw = await getDateInfo(symbol, "2025-10-10", "2025-10-26");
+    
+    //const priceHistoryRaw = await (await fetch("/aapl-ohlc-data.json")).json();
 
     // We know ahead of time the timespan is "day" and went from 2025-10-15 as far back as possible
 
@@ -29,7 +38,7 @@ export const load: PageServerLoad = async ({ params, fetch }) => {
         symbol: priceHistoryRaw.ticker,
         timespan: "day",
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        ohlc: priceHistoryRaw.results.map((v: any) => {
+        ohlc: priceHistoryRaw?.results.map((v: any) => {
             return [new Date(v.t), v.o, v.h, v.l, v.c];
         }),
     };
@@ -40,3 +49,31 @@ export const load: PageServerLoad = async ({ params, fetch }) => {
         priceHistory,
     };
 };
+
+
+async function getStockName(symbol : string) {
+  try {
+    const response = await rest.getTicker(symbol);
+    return response;
+  } catch (e) {
+    throw error(400, "Stock does not exist.");
+  }
+}
+
+
+
+async function getDateInfo(symbol: string, startDate: string, endDate : string) {
+  try {
+    const response = await rest.getStocksAggregates(
+      symbol,
+      1,
+      GetStocksAggregatesTimespanEnum.Day,
+      startDate,
+      endDate,
+      true
+    );
+    return response;
+  } catch (e) {
+    throw error(400, "Date inacessible.");
+  }
+}
