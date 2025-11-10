35ce76971af8923b39fd8b0888acd3a861b92f8b
Damon <57426668+BruhSoundEffectNumber2@users.noreply.github.com>
2025-11-10 14:55:39 -0800

    Added sql commands to create all db tables

---
+++ DatabaseSchemas.sql
@@ -0,0 +1,24 @@
+CREATE TABLE users (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    username varchar(32) UNIQUE NOT NULL,
+    name varchar(64) NOT NULL,
+    password bytea NOT NULL
+);
+
+CREATE TABLE sessions (
+    id varchar(32) PRIMARY KEY,
+    secret_hash bytea NOT NULL,
+    user_id uuid NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    created_at timestamp without time zone NOT NULL
+);
+
+CREATE TABLE stocks (
+    symbol varchar(16) PRIMARY KEY,
+    name varchar(128) NOT NULL
+);
+
+CREATE TABLE watchlists (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    stock varchar(16) NOT NULL REFERENCES stocks(symbol) ON DELETE CASCADE,
+    user_id uuid NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
